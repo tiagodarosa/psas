b94571120f0f69b1faa240bc7c7cf80328fb4f0a
Tiago da Rosa Santos <tiagodarosa@me.com>
2019-03-17 10:07:43 -0300

    Teste4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Página inicial mais um teste!';
+  title = 'Em desenvolvimento!';
 }
